ea0b32c13b842476065c04056ed3eb9bbd6afc3d
AngelMendez <angelmen1028@gmail.com>
2024-09-09 21:34:14 -0600

    Tarea 4/Unidad 2 terminada

---
+++ Coche.py
@@ -0,0 +1,17 @@
+class Coche:
+    marca = ""
+    modelo = ""
+    año = 0
+
+    def __init__(self, marca, modelo, año):
+        self.marca = marca
+        self.modelo = modelo
+        self.año = año
+    
+    def mostrar_info(self):
+        print("Marca del coche: ", self.marca)
+        print("Modelo del coche: ", self.modelo)
+        print("Año del coche: ", self.año)
+
+    def calcular_edad_coche(self):
+        print("Edad del coche : ", 2024-self.año)
---
+++ main.py
@@ -0,0 +1,17 @@
+from Coche import Coche
+
+coche_uno = Coche("Nissan", "Versa", 2015)
+coche_uno.mostrar_info()
+coche_uno.calcular_edad_coche()
+
+print("**********************************")
+
+coche_dos = Coche("Chevrolet", "Camaro", 1980)
+coche_dos.mostrar_info()
+coche_dos.calcular_edad_coche()
+
+print("**********************************")
+
+coche_tres = Coche("Volkswagen", "Polo", 2017)
+coche_tres.mostrar_info()
+coche_tres.calcular_edad_coche()
